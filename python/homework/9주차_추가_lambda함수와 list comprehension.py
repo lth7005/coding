18cde9638f43b0e4c4548b7068054a2c6c616eaa
lth7000 <lth7005@naver.com>
2020-05-23 16:51:19 +0900

    :)

--- python/homework/9주차_추가_lambda함수와 list comprehension.py
+++ python/homework/9주차_추가_lambda함수와 list comprehension.py
@@ -8,8 +8,8 @@ Created on Sat May 23 16:05:33 2020
 #lambda 함수와 list comprehension
 
 #1.lambda 함수, 람다함수는 이름없는 def 함수.
-n = lambda x,y : x + y #x,y를 인수로 가져와서 더한 값을 반환. 함수의 이름은 f
-k=n(4,5) #f라고 이름을 붙여서 사용할 수도 있다.
+n = lambda x,y : x + y #x,y를 인수로 가져와서 더한 값을 반환하는 이 함수의 이름은 n
+k=n(4,5) #이렇게 n이라고 이름을 붙여서 사용할 수도 있다.
 print(k) 
 
 #2.list comprehension, 
