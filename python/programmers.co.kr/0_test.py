ebc1a15fbe5e0793119e9ffe36858169991121b1
lth7000 <lth7005@naver.com>
2020-07-08 11:49:33 +0900

    :-)

---
+++ python/programmers.co.kr/0_test.py
@@ -0,0 +1,18 @@
+
+
+
+def solution(arrangement):
+
+    for i in arrangement:
+        if 
+
+
+
+
+
+    answer = 0
+    return answer
+
+#main
+stick = input('enter string : ')
+print(solution(stick))
